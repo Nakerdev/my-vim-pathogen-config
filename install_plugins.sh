bd9ba3d92ca03b8f852c2ade79676b741b9c4334
Naker90 <Antoniojesussg96@gmail.com>
2020-07-26 19:20:49 +0200

    delte vim markdoen plugin

--- install_plugins.sh
+++ install_plugins.sh
@@ -32,7 +32,6 @@ function clonePluginsRepositories() {
     git clone https://github.com/vim-airline/vim-airline.git ./bundle/vim-airline
     git clone https://github.com/preservim/nerdtree.git ./bundle/nerdtree
     git clone https://github.com/leafgarland/typescript-vim.git ./bundle/typescript-vim
-    git clone https://github.com/JamshedVesuna/vim-markdown-preview.git ./bundle/vim-markdown-preview
 }
 
 
--- plugin/vim-markdown-preview-mappings.vim
+++
@@ -1,1 +0,0 @@
-let vim_markdown_preview_hotkey='<C-v>m'
